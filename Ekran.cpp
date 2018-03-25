6c6b09cf91caae5d22a7d8efdc4584a0baca0538
kamil <kowalek1995@wp.pl>
2018-03-25 21:42:50 +0200

    zrobione

--- Ekran.cpp
+++ Ekran.cpp
@@ -10,11 +10,20 @@ Ekran::Ekran() {
        	//init_pair(5,COLOR_WHITE,COLOR_BLACK);
 	//init_pair(6,COLOR_BLACK,COLOR_BLACK);
 		
-	printw("Hello Worldd !!!");	/* Print Hello World		  */
-drawCegla(15,15,1);
+	printw("Hello Worldd !!!");
+	int table[100]; 	
+	drawCegla(15,15,1);
+	drawCegla(1,15,1);
+	int kolumna = rand() % 100 + 1;
+	drawCegla(1,kolumna,1);
+	int kolumna2 = rand() % 100 + 1;
+	drawCegla(1,kolumna2,1);
+	int 
 	refresh();			/* Print it on to the real screen */
 	getch();			/* Wait for user input */
 	endwin();			/* End curses mode		  */
+
+ 	
 	
 	
     }else{	
@@ -35,4 +44,19 @@ void Ekran::drawCegla(int row, int col, int t) {
     printw("*****");
     refresh();
     draw.unlock();
+}
+void Ekran::moveCegla(int row, int col, int t) {
+for(int i = 0; i<10;i++){
+    	draw.lock();
+	drawCegla(row+1,col+1,2);
+	attron(COLOR_PAIR(t));
+    	move(row,col);
+	row++;
+	col++;
+    	move(row,col);
+  	printw("*");
+sleep(1);
+  	refresh();
+  	draw.unlock();
+}
 }
--- go.sh
+++ go.sh
@@ -2,3 +2,5 @@ g++ -c -Wall -pedantic Main.cpp -pthread -lncurses -std=c++11
 g++ -c -Wall -pedantic Mur.cpp -pthread -lncurses -std=c++11
 g++ -c -Wall -pedantic Ekran.cpp -pthread -lncurses -std=c++11
 g++ -o program Main.o Ekran.o Mur.o -pthread -lncurses -std=c++11
+./program
+
